7f9dfdc530b5146fb809772c8bdb2344d26ded8f
Gustavo Azevedo Moura <gus.azmoura@gmail.com>
2021-10-04 21:14:22 -0300

    criando datasets tidys de renda e taxa desemprego

--- criando_datasets_tidy.R
+++ criando_datasets_tidy.R
@@ -4,7 +4,7 @@ library(plotly)
 library(openxlsx)
 
 #1) DATASET WIDE E TIDY PARA CRIMES LETAIS=========
-#(OCORR√äNCIAS DE HOM√çCIDIO DOLOSO + OCORR√äNCIAS DE LATROC√çNIO), apenas cidade de sp
+#(OCORR NCIAS DE HOMÕCIDIO DOLOSO + OCORR NCIAS DE LATROCÕNIO), apenas cidade de sp
 
 #dataset longer para ocorrencias de homicidio doloso
 crime_longer_doloso <- readr::read_csv("inputs_brutos/monthly_occurrences_policiaSP.csv") %>%
@@ -34,7 +34,7 @@ crime_longer_latrocinio <- readr::read_csv("inputs_brutos/monthly_occurrences_po
   dplyr::select(data_tidy, latrocinio) %>% 
   dplyr::arrange(data_tidy)
   
-#juntar os datasets e fazer a soma para ter ocorr√™ncias de crimes letais na cidade de sp
+#juntar os datasets e fazer a soma para ter ocorrÍncias de crimes letais na cidade de sp
 df_letais <- crime_longer_doloso %>% 
   dplyr::left_join(crime_longer_latrocinio, by = c("data_tidy")) %>% 
   dplyr::mutate(crimes_letais_SP = hom_doloso + latrocinio) %>% 
@@ -43,11 +43,9 @@ df_letais <- crime_longer_doloso %>%
 saveRDS(df_letais, "inputs_tidy/crimes_letais_SP.rds")
 #########
 
-
-
 #2) DATASET WIDE TIDY DE PRODUTIVIDADE POLICIAL =========
 
-#dataset wide com s√©ries de produtividade policial
+#dataset wide com sÈries de produtividade policial
 police_prcvty <- readr::read_csv("inputs_brutos/PoliceProductivity_policiaSP.csv") %>% 
   dplyr::select(-Total, -Regiao) %>% 
   dplyr::filter(Cidade == "S„o Paulo",
@@ -61,10 +59,26 @@ police_prcvty <- readr::read_csv("inputs_brutos/PoliceProductivity_policiaSP.csv
   dplyr::mutate(data_tidy = as.Date(paste0(Ano, "-", mes, "-01"), format("%Y-%m-%d"))) %>% 
   dplyr::select(-c(Ano, Cidade, mes)) %>% 
   dplyr::select(data_tidy, dplyr::everything()) %>% dplyr::arrange(data_tidy)
+
 saveRDS(police_prcvty, "inputs_tidy/police_productivity.rds")
 #########
 
-#3) DATASETS ATIVIDADE ECON√îMICA: DESEMPREGO E RENDIMENTO M√âDIO REAL DOS OCUPADOS
+#3) DATASETS ATIVIDADE ECON‘MICA: DESEMPREGO E RENDIMENTO M…DIO REAL DOS OCUPADOS=====
 
+#rendimento mÈdio real dos ocupados
+#obs: pegar planilha com nome Tab46
 
+economia <- readxl::read_excel("inputs_brutos/rendimento_real_mÈdio_dos_ocupados.xlsx", sheet = "Tab46") %>% 
+  tidyr::pivot_longer(cols = -c(Anos),
+                      names_to = "mes",
+                      values_to = "renda_real_ocup") %>% 
+  dplyr::mutate(mes = base::match(mes, month.name)) %>% 
+  dplyr::mutate(data_tidy = as.Date(paste0(Anos, "-", mes, "-01"), format("%Y-%m-%d"))) %>% 
+  dplyr::select(data_tidy, renda_real_ocup) %>% dplyr::arrange(data_tidy) %>% 
+  dplyr::left_join(readr::read_csv("inputs_brutos/desemprego_serie.csv", 
+                                   col_types = cols(...1 = col_skip())) %>% dplyr::select(data_tidy = Data, everything()),
+                   by = c("data_tidy")) %>% 
+  dplyr::rename(tx_desemp = "Taxa de desemprego")
 
+saveRDS(economia, "inputs_tidy/renda_desemprego_SP.rds")
+########
